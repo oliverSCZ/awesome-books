89d2bfbf1b7f73b8c192bccf1e1c79bac197b64f
Oliver Coimbra <olivercoimbra@gmail.com>
2021-11-29 18:12:09 -0400

    Add javascript file

---
+++ main.js
@@ -0,0 +1,12 @@
+function capturar() {
+  function Persona(book, author){
+      this.book = book;
+      this.author = author;
+  }
+
+var bookCapturar = document.getElementById('input-title').value;
+console.log(bookCapturar);
+
+
+}
+
